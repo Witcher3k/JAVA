822e590ea28f34e2ad1b3de4e257d89d02fb9e8f
Kamil Wiśniewski <Kamil Winiewski>
2017-05-11 20:35:15 +0200

    aktualizacja

--- pliki_zad3/src/com/company/Main.java
+++ pliki_zad3/src/com/company/Main.java
@@ -1,8 +1,56 @@
 package com.company;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	
+    static void sumujIZapisz(String nazwaPliku) throws IOException {
+        try {
+
+
+            FileReader fr = new FileReader(nazwaPliku);
+            BufferedReader br = new BufferedReader(fr);
+
+            FileWriter fw = new FileWriter(nazwaPliku, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            String tekst = "";
+            int licz = 0;
+
+            try {
+                do {
+                    try {
+                        tekst = br.readLine();
+                        licz = Integer.parseInt(tekst) + licz;
+                    } catch (NumberFormatException ex) {
+                        continue;
+                    }
+
+
+                } while (tekst != null);
+
+
+                bw.write("dopisane" + licz);
+                bw.newLine();
+            } finally {
+                br.close();
+                bw.close();
+
+            }
+
+        }catch(FileNotFoundException ex){ // w razie gdy nie ma pliku tworzymy nowy
+            try{
+                PrintWriter writer = new PrintWriter("plik.txt", "UTF-8");
+                writer.close();
+            } catch (IOException e) {
+                // do something
+            }
+        }
+    }
+
+    public static void main(String[] args)throws IOException {
+
+        sumujIZapisz("plik.txt");
+
+
     }
 }
